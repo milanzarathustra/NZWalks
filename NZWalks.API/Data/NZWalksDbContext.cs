67eea371365f9838110e088d31c47ef5724cb6d9
milan zarathustra <milanconhye@gmail.com>
2023-09-27 15:18:56 +0100

    formatting

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -7,7 +7,7 @@ namespace NZWalks.API.Data
     public class NZWalksDbContext: DbContext
     {
         public NZWalksDbContext(DbContextOptions<NZWalksDbContext> dbContextOptions) : base(dbContextOptions)
-        {                
+        {     
         }
 
         public DbSet<Difficulty> Difficulties { get; set; }
